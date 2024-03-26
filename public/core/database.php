1f0b4bc728645f7bb4c2653526f6026926f9c52c
leolopes2001 <leomartins2001@gmail.com>
2024-03-25 21:44:37 -0300

    Starting development of the ead platform

--- public/core/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$mysqli = new mysqli("db", "root", "1234", "db");
-
-if($mysqli)
--- public/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$mysqli = new mysqli("db", "root", "1234", "albuns");
-
-if ($mysqli->errno) {
-    die("Database error");
-}
